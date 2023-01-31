f5a54f5641933229402aca86a71d484547a0b848
Aryelle Siqueira <aryellesiqueira@hotmail.com>
2023-01-30 23:15:41 -0300

    topology 2 - rip

---
+++ start-topology-1.sh
@@ -0,0 +1,24 @@
+RTR="./rtr.jar"
+HWSW="./topology-1/"
+
+tmux new-session -d -s rare 
+
+tmux new-window 'java -jar '$RTR' routersc  '$HWSW'r1-hw.txt  '$HWSW'r1-sw.txt'
+
+tmux split-window -h
+tmux send 'java -jar '$RTR' routersc  '$HWSW'r2-hw.txt  '$HWSW'r2-sw.txt' ENTER;
+
+tmux split-window -h
+tmux send 'java -jar '$RTR' routersc  '$HWSW'r3-hw.txt  '$HWSW'r3-sw.txt' ENTER;
+
+tmux split-window -v
+tmux send 'java -jar '$RTR' routersc  '$HWSW'r4-hw.txt  '$HWSW'r4-sw.txt' ENTER;
+
+tmux split-window -v
+tmux send 'java -jar '$RTR' routersc  '$HWSW'r5-hw.txt  '$HWSW'r5-sw.txt' ENTER;
+
+tmux select-layout tiled;
+
+tmux a;
+
+### NOT WORKING YET
--- topology-1/start.sh
+++
@@ -1,5 +0,0 @@
-java -jar ../rtr.jar routersc ./r1-hw.txt ./r1-sw.txt
-java -jar ../rtr.jar routersc ./r2-hw.txt ./r2-sw.txt
-java -jar ../rtr.jar routersc ./r3-hw.txt ./r3-sw.txt
-java -jar ../rtr.jar routersc ./r4-hw.txt ./r4-sw.txt
-java -jar ../rtr.jar routersc ./r5-hw.txt ./r5-sw.txt
